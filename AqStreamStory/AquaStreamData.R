3f9c230381fabe89ba5e8198074260fa39c5b7cc
Joao Pita Costa <joaopitacosta@Joaos-MacBook-Air.local>
2016-05-19 21:58:39 +0200

    update

--- AqStreamStory/AquaStreamData.R
+++ AqStreamStory/AquaStreamData.R
@@ -1,24 +1,30 @@
 # Prepare data to upload in Stream Story
 # by Joao Pita Costa 2016
 
-# AquaStreamData <- read.csv("~/Documents/CODE/AquaSmartAnalytics/AquaStreamStory/AquaStreamData.csv", sep=";")
+# AquaStreamData <- read.csv("~/Documents/CODE/AquaSmartAnalytics/AquaStreamStory/input/AquaStreamData.csv", sep=";")
 # View(AquaStreamData)
 # format miliseconds column to numerical
-
+rm(AquaStreamData,Data)
+AquaStreamData <- ss_cmpaBreamSamplingClean
 Data<-AquaStreamData
-
+AquaStreamData <- AquaStreamData[order(AquaStreamData$Batch),]
+ View(AquaStreamData)
 L<-1
 N<-0
 batch<-AquaStreamData[L,5]
-while (L<nrow(AquaStreamData)+1) {
+while (L<nrow(AquaStreamData)) {
   if (AquaStreamData[L,5]==batch) {
-    L<-L+1
     AquaStreamData[L,1]<-AquaStreamData[L,1]+N
+    L<-L+1
   } else { 
     batch<-AquaStreamData[L,5]
     N<-N+10000000000000
+    AquaStreamData[L,1]<-AquaStreamData[L,1]+N
+    L<-L+1
   }
 }
-AquaStreamData<-AquaStreamData[1:nrow(AquaStreamData)-1,]
-write.csv(AquaStreamData, file = "AquaStreamData_upload.csv")  
+#AquaStreamData <- AquaStreamData[order(AquaStreamData$Middle),]
+#View(AquaStreamData)
+
+write.csv(AquaStreamData, file = "output/ss_cmpaBreamSamplingClean.csv")  
 
