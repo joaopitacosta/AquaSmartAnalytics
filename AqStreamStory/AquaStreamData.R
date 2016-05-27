68af686c99ad53ab78edcf88736bff8af897ad72
Joao Pita Costa <joaopitacosta@gmail.com>
2016-05-27 16:48:52 +0200

    update

--- AqStreamStory/AquaStreamData.R
+++ AqStreamStory/AquaStreamData.R
@@ -13,24 +13,30 @@ AquaStreamData <- AquaStreamData[order(AquaStreamData$Batch),]
 # View(AquaStreamData)
 L<-1
 N<-0
-Y<-AquaStreamData[L,11]
+T<-1000*60*60*24*356.25
+# Y<-AquaStreamData[L,11]
 batch<-AquaStreamData[L,5]
-while (L<nrow(AquaStreamData)) {
+while (L<nrow(AquaStreamData)+1) {
   if (AquaStreamData[L,5]==batch) {
     AquaStreamData[L,1]<-AquaStreamData[L,1]+N
-    AquaStreamData[L,11]<-Y
+    #AquaStreamData[L,11]<-Y
     L<-L+1
   } else { 
     batch<-AquaStreamData[L,5]
-    N<-N+10000000000000
-    Y<-Y+1
+    N<-N+T
+    #Y<-Y+1
     AquaStreamData[L,1]<-AquaStreamData[L,1]+N
-    AquaStreamData[L,11]<-Y
+    #AquaStreamData[L,11]<-Y
     L<-L+1
   }
 }
 #AquaStreamData <- AquaStreamData[order(AquaStreamData$Middle),]
 #View(AquaStreamData)
+
+AquaStreamData$Mortality.perc <- AquaStreamData$Mortality.No/AquaStreamData$Opening.Fish.No
+AquaStreamData$Transfer.perc <- AquaStreamData$Transfer....No./AquaStreamData$Opening.Fish.No
+AquaStreamData$Fasting.perc <- AquaStreamData$Fastings.No/AquaStreamData$Opening.Fish.No
+AquaStreamData$Harvest.perc <- AquaStreamData$Harvest..No./AquaStreamData$Opening.Fish.No
 
 write.csv(AquaStreamData, file = "output/ss_cmpaBreamSamplingClean.csv")  
 
--- AqStreamStory/AquaStreamFormat.R
+++ AqStreamStory/AquaStreamFormat.R
@@ -10,20 +10,20 @@ AquaStreamData$Transfer.perc <- AquaStreamData$Transfer....No./AquaStreamData$Op
 AquaStreamData$Fasting.perc <- AquaStreamData$Fastings.No/AquaStreamData$Opening.Fish.No
 AquaStreamData$Harvest.perc <- AquaStreamData$Harvest..No./AquaStreamData$Opening.Fish.No
 
-AquaStreamData$From <- NULL
-AquaStreamData$To <- NULL
-AquaStreamData$LTD.Mortality.No <- NULL
-AquaStreamData$LTD.Mortality.. <- NULL
-AquaStreamData$LTD.Econ..FCR <- NULL
-AquaStreamData$Transfer....Kg. <- NULL
-AquaStreamData$Transfer....Kg..1 <- NULL
-AquaStreamData$Transfer....No. <- NULL
-AquaStreamData$Transfer....No..1 <- NULL
-AquaStreamData$Sampling..No. <- NULL
-AquaStreamData$Harvest..No. <- NULL
-AquaStreamData$Mortality.No <- NULL
-AquaStreamData$Mortality.Deviation.... <- NULL
-AquaStreamData$Model.Mortality.No <- NULL
+#AquaStreamData$From <- NULL
+#AquaStreamData$To <- NULL
+#AquaStreamData$LTD.Mortality.No <- NULL
+#AquaStreamData$LTD.Mortality.. <- NULL
+#AquaStreamData$LTD.Econ..FCR <- NULL
+#AquaStreamData$Transfer....Kg. <- NULL
+#AquaStreamData$Transfer....Kg..1 <- NULL
+#AquaStreamData$Transfer....No. <- NULL
+#AquaStreamData$Transfer....No..1 <- NULL
+#AquaStreamData$Sampling..No. <- NULL
+#AquaStreamData$Harvest..No. <- NULL
+#AquaStreamData$Mortality.No <- NULL
+#AquaStreamData$Mortality.Deviation.... <- NULL
+#AquaStreamData$Model.Mortality.No <- NULL
 
 # View(AquaStreamData)
 
